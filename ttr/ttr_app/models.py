5658853f1a6d8583d31e6eb650303d588015a455
Rulo <solorulo@hotmail.com>
2015-03-04 13:00:21 -0600

    ibusqueda de instrumento por nivel de privacidad y tipo

--- ttr/ttr_app/models.py
+++ ttr/ttr_app/models.py
@@ -88,6 +88,7 @@ class InstrumentoEvaluacion(models.Model):
 	level_show = models.IntegerField(choices=TIPO_CHOICES, default=PLANTEL)
 
 	titulo = models.CharField(max_length=60, null=True, blank=True)
+	descripcion = models.CharField(max_length=65535, null=True, blank=True)
 	fecha_creacion = models.DateTimeField(auto_now_add=True)
 	autor = models.ForeignKey(MyUser, related_name="instrumento_autor")
 	asignatura = models.ForeignKey(Asignatura, null=True, blank=True)
@@ -102,14 +103,14 @@ class Rubrica (InstrumentoEvaluacion):
 	pass
 
 class CategoriaRubrica (models.Model):
-	texto = models.CharField(max_length=60, null=True, blank=True)
+	texto = models.CharField(max_length=65535, null=True, blank=True)
 	rubrica = models.ForeignKey(Rubrica)
 
 	def __unicode__(self):              # __unicode__ on Python 2
 		return self.texto
 
 class PonderacionRubrica (models.Model):
-	valor = models.IntegerField()
+	valor = models.CharField(max_length=65535, null=True, blank=True)
 	rubrica = models.ForeignKey(Rubrica)
 
 	def __unicode__(self):              # __unicode__ on Python 2
@@ -131,7 +132,8 @@ class ListaCotejo (InstrumentoEvaluacion):
 class IndicadorCotejo(models.Model):
 	listacotejo = models.ForeignKey(ListaCotejo)
 	texto = models.CharField(max_length=60, null=True, blank=True)
-	check = models.BooleanField(default=False) #definir valor maximo y minimo #valor hecho, no realizado, pendiente
+	check = models.BooleanField(default=False) 
+	observaciones = models.CharField(max_length=65535, null=True, blank=True)
 
 	def __unicode__(self):              # __unicode__ on Python 2
 		return self.texto
